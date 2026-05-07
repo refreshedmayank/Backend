b4aef0ae706b53dc17d4dffbc78c3fe2abd142e7
mynkq <refreshedmayank@gmail.com>
2026-05-07 18:18:50 +0530

    feat - updated readme added express.json middleware to read data need fixing errors

--- application/Readme.md
+++ application/Readme.md
@@ -3,4 +3,4 @@ Node_modules: folder where all the packages get installed.
 Api: Rules and protocols used for software communication. 
 express.js: Nodejs framework used for building web applications and Apis. 
 npm: node package manager. 
-
+postman: act as frontend helps in building and testing apis. 
--- application/src/app.js
+++ application/src/app.js
@@ -3,4 +3,10 @@ const express = require("express") // requiring express
 const app = express() // storing express call inside app variable
 // const port = 3000 // declaring port number 
 // app.listen(port) // listening app on declared port 
+app.use(express.json()) //middleware, read the data 
+const notes =[]
+app.post('/notes', (req, res)=>{
+const Note = req.body // storing notes body inside Note variable
+notes.push(Note)
+})
 module.exports = app // exporting the created code incase to be imported by another file. 
