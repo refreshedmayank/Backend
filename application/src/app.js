ca94166431c438ca980fe030f53f2a62df31fd49
mynkq <refreshedmayank@gmail.com>
2026-05-11 14:15:06 +0530

    Created api to update

--- application/src/app.js
+++ application/src/app.js
@@ -22,10 +22,21 @@ app.get('/notes',(req, res)=>{
 // Delete Api - delete/notes/:index (delete/notes/:1,2,3....1000+ its called params which is dynamic)
 app.delete('/notes/:index', (req, res)=>{
     const Index = req.params.index
-    delete Notes[Index]
+     Notes.slice(Index, 1)
     res.status(200).json({
         "Message": "Note Deleted Succesfully"
     })
+})
+// pATCH Api - used to update
+app.patch('/notes/:index', (req, res)=>{
+    const Index = req.params.index
+    const New_description = req.body.description
+    Notes[Index].description = New_description
+    res.status(200).json({
+        "Message": "The Note description is now updated"
+    })
+
+
 })
 module.exports = app 
 // exporting the created code incase to be imported by another file. node
