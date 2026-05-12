40b41c690568d2beaf80878f2797fe4c79f457bf
mynkq <refreshedmayank@gmail.com>
2026-05-12 15:52:52 +0530

    added Databaseconnection folder

---
+++ Databaseconnection/server.js
@@ -0,0 +1,4 @@
+const app = require("./src/app")
+app.listen(3000,()=>{
+    console.log("The server has been started at port 3000")
+})
---
+++ Databaseconnection/src/Database/Db.js
@@ -0,0 +1,4 @@
+const mongoose = require("mongoose")
+async function Db_connection(){
+    await mongoose.connect("mongodb+srv://refreshedmayank_db_user:ndFUyEGMpq6Be2dy@cluster0.f7vkhcs.mongodb.net/my_first_db")
+}
